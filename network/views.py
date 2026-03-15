63b02c3e81d8b2faf12c2848a7e38245a9502fe3
user <d_k81@mail.ru>
2026-03-15 20:11:37 +0500

    добавил CRUD API для сетевых узлов и продуктов

--- network/views.py
+++ network/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import NetworkNode, Product
+from .serializers import (
+    NetworkNodeCreateSerializer,
+    NetworkNodeDetailSerializer,
+    NetworkNodeListSerializer,
+    NetworkNodeUpdateSerializer,
+    ProductSerializer,
+)
+
+
+class NetworkNodeViewSet(ModelViewSet):
+    queryset = NetworkNode.objects.select_related("supplier").prefetch_related("products")
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return NetworkNodeCreateSerializer
+        if self.action in ("update", "partial_update"):
+            return NetworkNodeUpdateSerializer
+        if self.action == "retrieve":
+            return NetworkNodeDetailSerializer
+        return NetworkNodeListSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.select_related("network_node")
+    serializer_class = ProductSerializer
+    
